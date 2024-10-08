1fa2c1e697de9ef065ea0a3965541add9b426037
Gustavo Alonso Gutierrez <136649737+GustavoAG7201@users.noreply.github.com>
2024-10-08 12:47:44 -0700

    8/Octubre/2024
    Listado en carrusel de sneakers

--- src/app/content/inicio/inicio.component.ts
+++ src/app/content/inicio/inicio.component.ts
@@ -12,4 +12,14 @@ export class InicioComponent {
       '/imagenes/sneaker2.jpg',
       '/imagenes/sneaker3.jpg'
     ]
+
+     imagenes2 = [
+      {url:'/imagenes/sneaker4.jpg', texto: 'Sneaker1'},
+      {url:'/imagenes/sneaker5.jpg', texto: 'Sneaker2'},
+      {url:'/imagenes/sneaker6.jpg', texto: 'Sneaker3'},
+      {url:'/imagenes/sneaker7.jpg', texto: 'Sneaker4'},
+      {url:'/imagenes/sneaker8.jpg', texto: 'Sneaker5'}
+    ]
+
+    
 }
