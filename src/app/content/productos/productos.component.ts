aee7c6d8989bdab9c56d7a39464c6997dd02b80f
Gustavo Alonso Gutierrez <136649737+GustavoAG7201@users.noreply.github.com>
2024-10-15 12:46:09 -0700

    Avances en productos

--- src/app/content/productos/productos.component.ts
+++ src/app/content/productos/productos.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class ProductosComponent {
 
+    productos = [
+      {imagen: '/imagenes/sneaker9.jpg', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'},
+      {imagen: '/imagenes/sneaker10.jpg', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'},
+      {imagen: '/imagenes/sneaker11.png', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'},
+      {imagen: '/imagenes/sneaker12.png', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'},
+      {imagen: '/imagenes/sneaker13.png', descripcion: 'toyota', precio: '$mil ocho mil'},
+      {imagen: '/imagenes/sneaker4.jpg', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'},
+      {imagen: '/imagenes/sneaker5.jpg', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'},
+      {imagen: '/imagenes/sneaker6.jpg', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'},
+      {imagen: '/imagenes/sneaker7.jpg', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'},
+      {imagen: '/imagenes/sneaker8.jpg', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'}
+    ]
 }
