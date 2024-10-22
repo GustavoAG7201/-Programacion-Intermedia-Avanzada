c681dec353de734aa13f062c9b102398cc00cf87
Gustavo Alonso Gutierrez <136649737+GustavoAG7201@users.noreply.github.com>
2024-10-21 23:55:08 -0700

    21/Octubre/2024
    Agregado de las imagenes de productos
    Se agrego la talla del producto
    Falta modificar los textos descriptivos
    
    Estilos para resolver los problemas de resolucion:
    
    img{
        width: 100%;
        height: 100%;
        object-fit: cover;
    }

--- src/app/content/productos/productos.component.ts
+++ src/app/content/productos/productos.component.ts
@@ -8,15 +8,15 @@ import { Component } from '@angular/core';
 export class ProductosComponent {
 
     productos = [
-      {imagen: '/imagenes/sneaker9.jpg', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'},
-      {imagen: '/imagenes/sneaker10.jpg', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'},
-      {imagen: '/imagenes/sneaker11.png', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'},
-      {imagen: '/imagenes/sneaker12.png', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'},
-      {imagen: '/imagenes/sneaker13.png', descripcion: 'toyota', precio: '$mil ocho mil'},
-      {imagen: '/imagenes/sneaker4.jpg', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'},
-      {imagen: '/imagenes/sneaker5.jpg', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'},
-      {imagen: '/imagenes/sneaker6.jpg', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'},
-      {imagen: '/imagenes/sneaker7.jpg', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'},
-      {imagen: '/imagenes/sneaker8.jpg', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'}
+      {imagen: '/imagenes/producto1.png', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil', tallas: 'Tallas: 6,7,8,9,10,11'},
+      {imagen: '/imagenes/producto2.png', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil', tallas: 'Tallas: 6,7,8,9,10,11'},
+      {imagen: '/imagenes/producto3.png', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil', tallas: 'Tallas: 6,7,8,9,10,11'},
+      {imagen: '/imagenes/producto4.png', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil', tallas: 'Tallas: 6,7,8,9,10,11'},
+      {imagen: '/imagenes/producto5.png', descripcion: 'toyota', precio: '$mil ocho mil', tallas: 'Tallas: 6,7,8,9,10,11'},
+      {imagen: '/imagenes/producto6.png', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil'},
+      {imagen: '/imagenes/producto7.png', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil', tallas: 'Tallas: 6,7,8,9,10,11'},
+      {imagen: '/imagenes/producto8.png', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil', tallas: 'Tallas: 6,7,8,9,10,11'},
+      {imagen: '/imagenes/producto9.png', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil', tallas: 'Tallas: 6,7,8,9,10,11'},
+      {imagen: '/imagenes/producto10.png', descripcion: 'eta tu no la tiene, nah', precio: '$mil ocho mil', tallas: 'Tallas: 6,7,8,9,10,11'}
     ]
 }
