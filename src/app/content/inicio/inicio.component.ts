e1d0551caf76c8e6ebb18f73bbe92190e35846af
Gustavo Alonso Gutierrez <136649737+GustavoAG7201@users.noreply.github.com>
2024-11-09 00:36:49 -0800

    9-nov-2024
    Arreglos de estilos en todas las paginas
    Subida a firebase

--- src/app/content/inicio/inicio.component.ts
+++ src/app/content/inicio/inicio.component.ts
@@ -14,19 +14,19 @@ export class InicioComponent {
     ]
 
      imagenes2 = [
-      {url:'/imagenes/sneaker4.jpg', texto: 'Sneaker1'},
-      {url:'/imagenes/sneaker5.jpg', texto: 'Sneaker2'},
-      {url:'/imagenes/sneaker6.jpg', texto: 'Sneaker3'},
-      {url:'/imagenes/sneaker7.jpg', texto: 'Sneaker4'},
-      {url:'/imagenes/sneaker8.jpg', texto: 'Sneaker5'}
+      {url:'/imagenes/sneaker4.jpg', texto: 'Agregar al carrito'},
+      {url:'/imagenes/sneaker5.jpg', texto: 'Agregar al carrito'},
+      {url:'/imagenes/sneaker6.jpg', texto: 'Agregar al carrito'},
+      {url:'/imagenes/sneaker7.jpg', texto: 'Agregar al carrito'},
+      {url:'/imagenes/sneaker8.jpg', texto: 'Agregar al carrito'}
     ]
 
     imagenes3 = [
-      {url:'/imagenes/sneaker9.jpg', texto: 'Sneaker9'},
-      {url:'/imagenes/sneaker10.jpg', texto: 'Sneaker10'},
-      {url:'/imagenes/sneaker11.png', texto: 'Sneaker11'},
-      {url:'/imagenes/sneaker12.png', texto: 'Sneaker12'},
-      {url:'/imagenes/sneaker13.png', texto: 'Sneaker13'}
+      {url:'/imagenes/sneaker9.jpg', texto: 'Agregar al carrito'},
+      {url:'/imagenes/sneaker10.jpg', texto: 'Agregar al carrito'},
+      {url:'/imagenes/sneaker11.png', texto: 'Agregar al carrito'},
+      {url:'/imagenes/sneaker12.png', texto: 'Agregar al carrito'},
+      {url:'/imagenes/sneaker13.png', texto: 'Agregar al carrito'}
     ]
     
 }
